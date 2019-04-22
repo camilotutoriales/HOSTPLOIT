94755bcc4543ec8c47b48495ff42f854ea0cc965
camilotutoriales <46059590+camilotutoriales@users.noreply.github.com>
2019-04-22 05:25:08 -0500

    Instalador de la dependencias

---
+++ install.sh
@@ -0,0 +1,15 @@
+#Paquetes:
+#Instalaciones!!
+apt update && apt upgrade -y
+pkg install curl -y
+pkg install figlet -y
+pkg install util-linux -y
+pkg install ruby -y
+gem install lolcat
+pkg install python2 -y
+pkg install python -y
+pip install speedtest-cli
+sleep 2.5
+figlet -f mini FIN | lolcat
+#fin
+#Instalacion exitosa Dependencias :")
